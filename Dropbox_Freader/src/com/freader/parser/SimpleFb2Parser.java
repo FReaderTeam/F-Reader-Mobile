980af2f69467385f8972f8734347d5f874aac27d
artemiy619 <artem.sh2008@gmail.com>
2014-11-09 16:53:32 +0200

    split tags from words

--- Dropbox_Freader/src/com/freader/parser/SimpleFb2Parser.java
+++ Dropbox_Freader/src/com/freader/parser/SimpleFb2Parser.java
@@ -87,7 +87,7 @@ public class SimpleFb2Parser {
                 if (title.find()) {
                     paragraph = SOP.fb2Paragraph.matcher(title.group(0));
                     while (paragraph.find()) {
-                        col.add("%title "
+                        col.add(" %title "
                                 + paragraph.group(0).replaceAll("<.*?>", ""));
                     }
                 }
@@ -107,12 +107,12 @@ public class SimpleFb2Parser {
                     StringBuilder s = new StringBuilder();
                     if (title.find()) {
                         if (newSection) {
-                            s.append("%new-section ");
+                            s.append(" %new-section ");
                             newSection = false;
                         }
                         paragraph = SOP.fb2Paragraph.matcher(title.group(0));
                         while (paragraph.find()) {
-                            s.append("%title ");
+                            s.append(" %title ");
                             s.append(paragraph.group(0).replaceAll("<.*?>", ""));
                             col.add(s.toString());
                             paragraphIndexer++;
